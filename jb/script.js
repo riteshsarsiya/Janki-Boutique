61f6088e9ba61df69226debc38aaa89f98d35b4a
riteshsarsiya <riteshsarsiya3@gmail.com>
2026-08-05 14:25:44 +0530

    T-shirt QR product system

---
+++ jb/script.js
@@ -0,0 +1,29 @@
+const products = {
+    TS001: {
+        name: "Black T-Shirt",
+        size: "XL",
+        price: "₹799",
+        image: "/img/download.png"
+    }, 
+
+    TS002: {
+        name: "White T-Shirt",
+        size: "L",
+        price: "₹699",
+        image: "/img/download1.png"
+    }
+}
+// QR se ID milegi (example: ?id=TS001)
+const id = new URLSearchParams(window.location.search).get("id");
+
+const product = products[id];
+
+if(product) {
+
+    document.querySelector("#product-name").textContent = "Name : " + product.name;
+    document.querySelector("#product-size").textContent = "Size : " + product.size;
+    document.querySelector("#product-price").textContent = "Price : " + product.price;
+    document.querySelector(".cloth-png").src = product.image;
+} else {
+    document.querySelector("body").innerHTML = "Product Not Found";
+}
