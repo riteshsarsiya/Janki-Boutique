9cbc8bf757d30a48f61bbab28879cf1e9aca0ef9
riteshsarsiya <riteshsarsiya3@gmail.com>
2026-08-05 23:13:25 +0530

    Fix image path

--- jb/fix-json.js
+++ jb/fix-json.js
@@ -3,9 +3,14 @@ const fs = require("fs");
 const data = JSON.parse(fs.readFileSync("products.json", "utf8"));
 
 data.forEach(product => {
-  product.Image = product.Image.replace("/jb/img/img/", "/jb/img/");
+  const fileName = product.Image.split(/[\\/]/).pop(); // sirf file name nikalega
+  product.Image = `/jb/img/${fileName}`;
 });
 
-fs.writeFileSync("products.json", JSON.stringify(data, null, 2), "utf8");
+fs.writeFileSync(
+  "products.json",
+  JSON.stringify(data, null, 2),
+  "utf8"
+);
 
-console.log("✅ Done");
+console.log("✅ Image paths fixed");
